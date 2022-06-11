9ebfb233e213d17bfe701693235a2d6f49fc9da2
Oskar Georgsson <oskar.georgsson@gmail.com>
2022-06-11 17:21:54 +0200

    Migration for removing the user name and adding a user role

--- StringManager.Infrastructure/Migrations/20220611152104_RemovedUserName.Designer.cs
+++ StringManager.Infrastructure/Migrations/20220611152104_RemovedUserName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StringManager.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using StringManager.Infrastructure.Persistence;
 namespace StringManager.Infrastructure.Migrations
 {
     [DbContext(typeof(StringManagerDbContext))]
-    partial class StringManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220611152104_RemovedUserName")]
+    partial class RemovedUserName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StringManager.Infrastructure/Migrations/20220611152104_RemovedUserName.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StringManager.Infrastructure.Migrations
+{
+    public partial class RemovedUserName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Forename",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "User");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserRole",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRole",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Forename",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
